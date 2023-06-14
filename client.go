e1f2cc55a850e25fb00caebb3ad01e6929b592ef
kristof1345 <kristofnemeth1345@gmail.com>
2023-06-14 16:18:08 +0200

    client

--- client.go
+++ client.go
@@ -1,1 +1,40 @@
 package main
+
+import "github.com/gorilla/websocket"
+
+//client is a single chatting user
+
+type client struct {
+	socket *websocket.Conn
+
+	// channel for messages
+	recieve chan []byte
+
+	//the room the client is chatting in
+	room *room
+}
+
+type room struct {
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+
+	for msg := range c.recieve {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
